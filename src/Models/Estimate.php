5d00c00114422ad90cf842fe3dd2de6abfb91d04
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-09-19 18:44:25 +0300

    Update - tenant scoping - update globals

--- src/Models/Estimate.php
+++ src/Models/Estimate.php
@@ -4,7 +4,7 @@ namespace Rutatiina\Estimate\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
-use App\Scopes\TenantIdScope;
+use Rutatiina\Tenant\Scopes\TenantIdScope;
 
 class Estimate extends Model
 {
